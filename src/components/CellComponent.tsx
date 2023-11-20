873aecea73d26a20dcbbb037c070d54fe2aa2c21
Karlen Misakyan <misakyankarlen@gmail.com>
2023-11-20 22:47:16 +0300

    can move

--- src/components/CellComponent.tsx
+++ src/components/CellComponent.tsx
@@ -1,29 +1,28 @@
-import React, { FC } from "react"
-import { Colors } from "../models/Colors"
-import { Cell } from "../models/Cell"
-import { FigureNames } from "../models/figures/Figure"
+import React, { FC } from "react";
+import { Colors } from "../models/Colors";
+import { Cell } from "../models/Cell";
+import { FigureNames } from "../models/figures/Figure";
 
 interface CellProps {
-  color: Colors
-  cell: Cell
-  selected: boolean
-  onClick: (cell: Cell) => void
+  color: Colors;
+  cell: Cell;
+  selected: boolean;
+  onClick: (cell: Cell) => void;
 }
 
 const CellComponent: FC<CellProps> = ({ color, cell, selected, onClick }) => {
+  const isUnderAttack = cell.available && cell.figure;
   return (
     <div
       onClick={() => onClick(cell)}
       className={`cell ${color} ${
         cell.figure?.name === FigureNames.PAWN ? "pawn" : ""
-      } ${cell.available && cell.figure ? "under-attack" : ""} ${
-        selected ? "selected" : ""
-      }`}
+      } ${isUnderAttack ? "under-attack" : ""} ${selected ? "selected" : ""}`}
     >
-      {cell.available ? <div className="available" /> : null}
+      {cell.available && !isUnderAttack ? <div className="available" /> : null}
       {cell.figure?.logo && <img src={cell.figure.logo} />}
     </div>
-  )
-}
+  );
+};
 
-export default CellComponent
+export default CellComponent;
--- src/models/Board.ts
+++ src/models/Board.ts
@@ -1,6 +1,6 @@
 import { Cell } from "./Cell";
 import { Colors } from "./Colors";
-import { Bishop } from './figures/Bishop';
+import { Bishop } from "./figures/Bishop";
 import { King } from "./figures/King";
 import { Knight } from "./figures/Knight";
 import { Pawn } from "./figures/Pawn";
@@ -15,7 +15,7 @@ export class Board {
       const row: Cell[] = [];
       for (let j = 0; j < 8; j++) {
         if ((i + j) % 2 !== 0) {
-          row.push(new Cell(this, j, i, Colors.BLACK, null)); //черные
+          row.push(new Cell(this, j, i, Colors.BLACK, null)); // черные
         } else {
           row.push(new Cell(this, j, i, Colors.WHITE, null)); // белые
         }
@@ -44,13 +44,12 @@ export class Board {
     }
   }
   private addKings() {
-    new King(Colors.BLACK, this.getCell(4,0))
-    new King(Colors.WHITE, this.getCell(4,7))
+    new King(Colors.BLACK, this.getCell(4, 0));
+    new King(Colors.WHITE, this.getCell(4, 7));
   }
   private addQueens() {
-    new Queen(Colors.BLACK, this.getCell(3,0))
-    new Queen(Colors.WHITE, this.getCell(3,7))
-
+    new Queen(Colors.BLACK, this.getCell(3, 0));
+    new Queen(Colors.WHITE, this.getCell(3, 7));
   }
   private addRooks() {
     new Rook(Colors.BLACK, this.getCell(0, 0));
@@ -67,19 +66,19 @@ export class Board {
     new Knight(Colors.WHITE, this.getCell(6, 7));
   }
   private addBishops() {
-    new Bishop(Colors.BLACK, this.getCell(2,0))
-    new Bishop(Colors.BLACK, this.getCell(5,0))
+    new Bishop(Colors.BLACK, this.getCell(2, 0));
+    new Bishop(Colors.BLACK, this.getCell(5, 0));
 
-    new Bishop(Colors.WHITE, this.getCell(2,7))
-    new Bishop(Colors.WHITE, this.getCell(5,7))
+    new Bishop(Colors.WHITE, this.getCell(2, 7));
+    new Bishop(Colors.WHITE, this.getCell(5, 7));
   }
 
   public highlightCells(selectedCell: Cell | null) {
     for (let i = 0; i < this.cells.length; i++) {
-      const row = this.cells[i]
+      const row = this.cells[i];
       for (let j = 0; j < row.length; j++) {
         const target = row[j];
-        target.available = !!selectedCell?.figure?.canMove(target)
+        target.available = !!selectedCell?.figure?.canMove(target);
       }
     }
   }
